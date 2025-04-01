dd2f8b55e852601d4452c3a702a4b123dc37007b
Luiz Augusto do Amaral Ferrete - LZF <luiz.ferrete@iteris.com.br>
2025-03-31 22:55:14 -0300

    Moving to a new file the endpoint.

---
+++ authentication-app/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,30 @@
+﻿namespace AuthenticationApp.Endpoints
+{
+    public static class WeatherForecastEndpoints
+    {
+        public static IEndpointRouteBuilder MapWeatherForecastEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            routes.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            return routes;
+        }
+    }
+}
